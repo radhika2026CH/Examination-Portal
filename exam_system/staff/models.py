3253348054017a9ae66e86a2cad2285715be73e6
radhika2026CH <radhika.gupta@livehealth.in>
2022-10-18 14:32:18 +0530

    staff student

--- exam_system/staff/models.py
+++ exam_system/staff/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from phonenumber_field.modelfields import PhoneNumberField
 
     
 
--- exam_system/student/models.py
+++ exam_system/student/models.py
@@ -8,7 +8,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 class Student(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     phone_number = PhoneNumberField(blank=False, unique=True)
-    email = models.EmailField(max_length=254, null=False, blank=False, unique=True)
+    email = models.EmailField(max_length=254, null=False, blank=False)
     active = models.BooleanField(default = True, null= False, blank=False)
     PRN = models.IntegerField(null=False, blank=False)
     def __str__(self):
